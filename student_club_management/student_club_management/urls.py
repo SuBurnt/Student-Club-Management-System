8ada64bc48637b6fffb97503dcf7643b4c4fccd2
SuBurnt <sm2349785@gmail.com>
2026-06-26 00:49:24 +0700

    Добавлены пути и кастомный аккаунт

--- student_club_management/student_club_management/urls.py
+++ student_club_management/student_club_management/urls.py
@@ -15,8 +15,20 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('accounts/', include('apps.accounts.urls')),
+    path('clubs/', include('apps.clubs.urls')),
+    path('events/', include('apps.events.urls')),
+    path('discussions/', include('apps.discussions.urls')),
+    path('', TemplateView.as_view(template_name='home.html'), name='home'),
 ]
+
+# Для медиа-файлов (аватарки)
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
